d904798c6e078a7c012661b2a057b41673b4c851
rizaptk <rizaptk@outlook.com>
2026-02-23 10:17:39 +0000

    new updates

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/lib/firestore-sqlite/worker.ts'],
+  entry: ['src/index.ts'],
   format: ['esm'],
   dts: { entry: ['src/index.ts'] },
   sourcemap: false,
   clean: true,
   splitting: false,
-  bundle: true,
-  external: [],
   minify: false,
   outDir: 'dist',
   target: 'es2022'
